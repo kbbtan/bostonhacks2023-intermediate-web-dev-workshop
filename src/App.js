b13f12243df7f3a90c35e2e2b4da42deafd6c246
Kian Boon <kianboon@yctan.com>
2023-11-18 01:26:52 -0500

    React Components for reusing code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-    </div>  
+      <h1>My Grocery List</h1>
+
+      <Card 
+        title="Spring Onions" 
+        description="I probably should buy more of them" 
+        price="2" 
+      />
+
+      <Card 
+        title="Salt and Pepper" 
+        description="A must for any kitchen" 
+        price="5" 
+      />
+
+      <Card 
+        title="Chicken Thighs" 
+        description="Good source of protein" 
+        price="8" 
+      />
+    </div>
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,11 @@
-function Card() {
+function Card({ title, description, price }) {
   return (
-    <div>
-        Card
+    <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">{title}</h5>
+          <p class="card-text">{description}</p>
+          <p class="card-text"><small class="text-body-secondary">${price}</small></p>
+        </div>
     </div>
   )
 }
