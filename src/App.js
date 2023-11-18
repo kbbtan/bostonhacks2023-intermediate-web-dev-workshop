9554363b5987263a775f16a8628ba2daa252a485
jamxiao2025 <95207937+jamxiao2025@users.noreply.github.com>
2023-11-18 14:09:41 -0500

    adding files

--- src/App.js
+++ src/App.js
@@ -2,35 +2,115 @@ import './App.css';
 import Card from './Card';
 
 import { useState } from 'react';
+import { useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, addDoc, Timestamp, deleteDoc, doc } from 'firebase/firestore/lite';
+import FirebaseConfig from "./firebase.js";
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
+
+const app = initializeApp(FirebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+const getGroceries = async(db) => {
+  const groceries = collection(db, 'groceries');
+  const out = await getDocs(groceries);
+  // const groceryList = out.docs.map(doc => ({ id: doc.id, data: doc.data() }));
+  return out;
+}
+
+const createGrocery = async(db, data) => {
+  try {
+    await addDoc(collection(db, 'groceries'), {
+      name: data.name,
+      quantity: data.quantity,
+      price: data.price,
+      completed: false,
+      created: Timestamp.now()
+    })
+  } catch (err) {
+    alert(err)
+  }
+}
 
 function App() {
-  const [groceries, setGroceries] = useState([
-    {
-      title: "Tofu",
-      description: "Good for soup",
-      price: "5"
-    },
-    {
-      title: "Salt and Pepper",
-      description: "A must for any kitchen",
-      price: "5"
-    },
-    {
-      title: "Chicken Thighs",
-      description: "Good source of protein",
-      price: "8"
-    }
-  ]);
+  const [groceries, setGroceries] = useState([]);
+  const [formFields, setFormFields] = useState({
+    name: "",
+    quantity: 1,
+    price: 0,
+    completed: false
+  })
+
+  useEffect(() => {
+
+    const fetchGroceries = async() => {
+      let out = await getGroceries(db);
+      
+      setGroceries(out.docs.map(doc => (
+        {
+          id: doc.id,
+          data: doc.data()
+        }
+      )));
+      console.log();
+    } 
+
+    fetchGroceries();
+  }, [groceries])
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormFields(prevFields => ({
+      ...prevFields,
+      [name]: value
+    }));
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    await createGrocery(db, formFields, () => setFormFields({
+      name: "",
+      quantity: 1,
+      price: 0,
+      completed: false
+    }));
+  }
+
 
   return (
+
     <div className="App">
       <h1>My Grocery List</h1>
 
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" value={formFields.name} onChange={handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Quantity:
+          <input type="number" name="quantity" value={formFields.quantity} onChange={handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Price:
+          <input type="number" name="price" value={formFields.price} onChange={handleInputChange} />
+        </label>
+        <br />
+        <button type="submit">Add Grocery</button>
+      </form>
+
       {groceries.map((grocery) => {
         return <Card 
-          title={grocery.title} 
-          description={grocery.description} 
-          price={grocery.price} 
+          id={grocery.id}
+          title={grocery.data.name} 
+          description={grocery.data.quantity} 
+          price={grocery.data.price} 
+          db={db}
         />
       })}
     </div>
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDcHXY4gwnkcvC-Fx6K4EGv1GdWweEIxd4",
+  authDomain: "bhacksworkshop.firebaseapp.com",
+  projectId: "bhacksworkshop",
+  storageBucket: "bhacksworkshop.appspot.com",
+  messagingSenderId: "796664372053",
+  appId: "1:796664372053:web:df19fbb55c2e4bc13b2ddc",
+  measurementId: "G-0VVFV5ZQ90"
+};
+
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export default firebaseConfig;
