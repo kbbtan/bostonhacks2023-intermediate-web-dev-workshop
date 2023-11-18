072085aca3d453dcd0606a1e8ff127c835084920
jamxiao2025 <95207937+jamxiao2025@users.noreply.github.com>
2023-11-18 14:09:49 -0500

    modified firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,9 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDcHXY4gwnkcvC-Fx6K4EGv1GdWweEIxd4",
-  authDomain: "bhacksworkshop.firebaseapp.com",
-  projectId: "bhacksworkshop",
-  storageBucket: "bhacksworkshop.appspot.com",
-  messagingSenderId: "796664372053",
-  appId: "1:796664372053:web:df19fbb55c2e4bc13b2ddc",
-  measurementId: "G-0VVFV5ZQ90"
+/*
+Your config here
+*/
 };
 
 
